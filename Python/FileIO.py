75a50e1ec0b3114a894811d667259cc70a12df10
mohamed basiouny <mohamed_elghayesh@hotmail.com>
2020-01-03 18:33:17 +0200

    files

--- Python/FileIO.py
+++ Python/FileIO.py
@@ -1,15 +1,19 @@
-import os
-
 # CWD is Atom so to read from Python subfolder
-""" write_to = open("Python\\Test.txt","w")
+# Creating new file then closing
+write_to = open("Python\\Test.txt","w")
 write_to.write("Hello World\nThis is a good example\nfor learning file methods")
 write_to.close()
 
+# reading the file then closing
 read_file = open("Python\\Test.txt","r")
 print([*read_file])
-read_file.close() """
+read_file.close()
 
+# appending to file then closing
 append_to = open("Python\\Test.txt","a")
-append_to.write("This should go to the last line.")
-print([*append_to])
+append_to.write("\nThis should go to the last line.")
 append_to.close()
+
+read_file = open("Python\\Test.txt","r")
+print([*read_file])
+read_file.close()
